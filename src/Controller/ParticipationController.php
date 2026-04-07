c3431357d1c6d3d60125b3bd92ea9a43594cdc75
Ramezcherni <MohamedRamez.Cherni@esprit.tn>
2026-04-07 19:12:03 +0100

    crud_cntrlsaisie

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -9,6 +9,7 @@ use App\Repository\FormationRepository;
 use App\Repository\ParticipationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
