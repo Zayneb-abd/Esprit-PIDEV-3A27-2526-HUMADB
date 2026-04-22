eed67aa6d880b9655bd8f1918857055f67d79bfc
nermine dziri <nerminedziri7@gmail.com>
2026-04-22 02:05:40 +0100

    autre bundle

--- src/Controller/EmployPublicationController.php
+++ src/Controller/EmployPublicationController.php
@@ -6,6 +6,7 @@ use App\Entity\Publication;
 use App\Entity\Commentaire;
 use App\Repository\ReactionPublicationRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,22 +19,44 @@ class EmployPublicationController extends AbstractController
 {
     private $reactionRepository;
     private $entityManager;
+    private $paginator;
 
-    public function __construct(ReactionPublicationRepository $reactionRepository, EntityManagerInterface $entityManager)
+    public function __construct(ReactionPublicationRepository $reactionRepository, EntityManagerInterface $entityManager, PaginatorInterface $paginator)
     {
         $this->reactionRepository = $reactionRepository;
         $this->entityManager = $entityManager;
+        $this->paginator = $paginator;
     }
 
     /**
      * Afficher la liste des publications avec les réactions
      */
     #[Route('/', name: 'employ_publication_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $publications = $this->entityManager->getRepository(Publication::class)
-            ->findBy([], ['datePublication' => 'DESC']);
+        // Paginer avec findBy simple
+        $page = $request->query->getInt('page', 1);
+        $limit = 5; // 5 publications par page
+        $offset = ($page - 1) * $limit;
+        
+        // Récupérer toutes les publications triées par ID DESC
+        $allPublications = $this->entityManager->getRepository(Publication::class)
+            ->findBy([], ['id' => 'DESC']);
+        
+        // Extraire seulement les publications pour la page actuelle
+        $publications = array_slice($allPublications, $offset, $limit);
+        
+        // Debug simple
+        dump([
+            'page' => $page,
+            'offset' => $offset,
+            'limit' => $limit,
+            'total_publications' => count($allPublications),
+            'count_publications_page' => count($publications),
+            'ids_page' => array_map(fn($p) => $p->getId(), $publications)
+        ]);
 
+        
         return $this->render('employ/publication/index.html.twig', [
             'publications' => $publications,
             'reaction_repository' => $this->reactionRepository
